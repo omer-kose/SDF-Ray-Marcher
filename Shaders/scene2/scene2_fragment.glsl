03e72632a39c3d30fcac7a11f1365975520ff97c
e2380269 <e2380269@ceng.metu.edu.tr>
2023-06-15 22:23:34 +0300

    iq's implementation works 1000 times better

--- Shaders/scene2/scene2_fragment.glsl
+++ Shaders/scene2/scene2_fragment.glsl
@@ -983,36 +983,57 @@ void translateCube(inout vec3 p, int xi, int yi, int zi, float cubeSize){
     p.z += zi*cubeSize*2;
 }
 
-float sponge2(vec3 p,float cubeSize){
-    float tempRes = 100000000;
-    for(int xi = -1 ; xi<2; ++xi){
-        for(int yi = -1 ; yi<2; ++yi){
-            if(xi == 0 && yi == 0){continue;}
-            for(int zi = -1 ; zi<2; ++zi){
-                if((xi == 0 && zi == 0) || (yi == 0 && zi == 0)){continue;}
-                vec3 pb = p;
-                translateCube(pb, xi, yi, zi, cubeSize);
-                tempRes = min(tempRes,fBoxCheap(pb, vec3(cubeSize)));
-            }
-        }
-    }
-    return tempRes;
+float maxcomp(in vec3 p ) { return max(p.x,max(p.y,p.z));}
+float sdBox( vec3 p, vec3 b )
+{
+    vec3  di = abs(p) - b;
+    float mc = maxcomp(di);
+    return min(mc,length(max(di,0.0)));
 }
 
-float sponge(vec3 p,float cubeSize){
-    float tempRes = 100000000;
-    for(int xi = -1 ; xi<2; ++xi){
-        for(int yi = -1 ; yi<2; ++yi){
-            if(xi == 0 && yi == 0){continue;}
-            for(int zi = -1 ; zi<2; ++zi){
-                if((xi == 0 && zi == 0) || (yi == 0 && zi == 0)){continue;}
-                vec3 pb = p;
-                translateCube(pb, xi, yi, zi, cubeSize);
-                tempRes = min(tempRes,sponge2(pb,cubeSize/3));
-            }
+vec2 iBox( in vec3 ro, in vec3 rd, in vec3 rad )
+{
+    vec3 m = 1.0/rd;
+    vec3 n = m*ro;
+    vec3 k = abs(m)*rad;
+    vec3 t1 = -n - k;
+    vec3 t2 = -n + k;
+    return vec2( max( max( t1.x, t1.y ), t1.z ),
+                 min( min( t2.x, t2.y ), t2.z ) );
+}
+
+const mat3 ma = mat3( 0.60, 0.00,  0.80,
+                      0.00, 1.00,  0.00,
+                     -0.80, 0.00,  0.60 );
+float map( in vec3 p )
+{
+    float d = sdBox(p,vec3(1.0));
+    vec4 res = vec4( d, 1.0, 0.0, 0.0 );
+
+    float ani = smoothstep( -0.2, 0.2, -cos(0.5) );
+    float off = 1.5*sin( 0.01 );
+    
+    float s = 1.0;
+    for( int m=0; m<4; m++ )
+    {
+        p = mix( p, ma*(p+off), ani );
+       
+        vec3 a = mod( p*s, 2.0 )-1.0;
+        s *= 3.0;
+        vec3 r = abs(1.0 - 3.0*abs(a));
+        float da = max(r.x,r.y);
+        float db = max(r.y,r.z);
+        float dc = max(r.z,r.x);
+        float c = (min(da,min(db,dc))-1.0)/s;
+
+        if( c>d )
+        {
+          d = c;
+          res = vec4( d, min(res.y,0.2*da*db*dc), (1.0+float(m))/4.0, 0.0 );
         }
     }
-    return tempRes;
+
+    return res.x;
 }
 
 /*
@@ -1022,7 +1043,7 @@ vec2 closest_object(vec3 p){
     
     float boxID = 1.0;
     vec2 res;
-    float tempRes = sponge(p,8);
+    float tempRes = map(p);
     
     res = fOpUnionID(res, vec2(tempRes, boxID));
         
